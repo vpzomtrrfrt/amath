9e9c47e01393c96765b57183fbd45063faea71d4
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-05-05 10:11:19 -0600

    Add more arithmetic operators

--- src/eval.rs
+++ src/eval.rs
@@ -4,7 +4,10 @@ impl Expression {
     pub fn eval(&self, context: &Context) -> Value {
         match *self {
             Expression::Const(ref x) => *x,
-            Expression::Multiply(ref a, ref b) => a.eval(context).multiply(b.eval(context))
+            Expression::Add(ref a, ref b) => a.eval(context).add(b.eval(context)),
+            Expression::Subtract(ref a, ref b) => a.eval(context).subtract(b.eval(context)),
+            Expression::Multiply(ref a, ref b) => a.eval(context).multiply(b.eval(context)),
+            Expression::Divide(ref a, ref b) => a.eval(context).divide(b.eval(context))
         }
     }
 }
@@ -14,6 +17,27 @@ impl Value {
         match *self {
             Value::Int(a) => match rh {
                 Value::Int(b) => Value::Int(a * b)
+            }
+        }
+    }
+    fn add(&self, rh: Value) -> Value {
+        match *self {
+            Value::Int(a) => match rh {
+                Value::Int(b) => Value::Int(a + b)
+            }
+        }
+    }
+    fn subtract(&self, rh: Value) -> Value {
+        match *self {
+            Value::Int(a) => match rh {
+                Value::Int(b) => Value::Int(a - b)
+            }
+        }
+    }
+    fn divide(&self, rh: Value) -> Value {
+        match *self {
+            Value::Int(a) => match rh {
+                Value::Int(b) => Value::Int(a / b)
             }
         }
     }
--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,10 @@
 #[derive(Debug)]
 pub enum Expression {
     Const(Value),
-    Multiply(Box<Expression>, Box<Expression>)
+    Add(Box<Expression>, Box<Expression>),
+    Subtract(Box<Expression>, Box<Expression>),
+    Multiply(Box<Expression>, Box<Expression>),
+    Divide(Box<Expression>, Box<Expression>)
 }
 
 #[derive(Debug, Clone, Copy)]
